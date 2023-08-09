6fd803871c4bc239b25151560fcddb5dfbd26042
김성은 <kse012050@naver.com>
2023-08-10 02:09:47 +0900

    PC - 어복황제 페이지 완료

--- js/common.js
+++ js/common.js
@@ -1,14 +1,44 @@
 $(document).ready(function(){
+    // css index
     $('[data-styleIdx]').length && styleIdx();
+
+    // 텝
+    $('[data-tab="btns"]').length && tab();
+
+    // 어복황제
+    $('.fishPage').length && fishPage();
 })
 
-
+// css index
 function styleIdx(){
     const selector = $('[data-styleIdx]');
     selector.each(function(){
         const attrValue = $(this).attr('data-styleIdx');
-        $(this).children(attrValue || attrValue).each(function(i){
+        $(this).find(attrValue ? attrValue : '> *').each(function(i){
             $(this).css('--styleIdx', i);
         })
     })
+}
+
+// 텝
+function tab(){
+    const attrKey = 'data-tab';
+    $(`[${attrKey}]`).each(function(){
+        $(this).children().first().addClass('active');
+    })
+    $(`[${attrKey}="btns"]`).children().click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        $(`[${attrKey}="contents"]`).children().eq($(this).index()).addClass('active').siblings().removeClass('active');
+    })
+}
+
+// 어복황제
+function fishPage(){
+    var fishSwiper = new Swiper(".fishSwiper", {
+        slidesPerView: 3,
+        navigation: {
+            prevEl: '.btn-prev',
+            nextEl: '.btn-next',
+        },
+    });
 }
